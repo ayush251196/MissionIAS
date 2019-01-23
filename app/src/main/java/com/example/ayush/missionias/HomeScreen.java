1e8b548dd6a4cb797d021dbeb7d43f2afd04ed49
jitu665 <jitendragupta665@gmail.com>
2019-01-23 19:05:29 +0530

    sharing the app change

--- app/src/main/java/com/example/ayush/missionias/HomeScreen.java
+++ app/src/main/java/com/example/ayush/missionias/HomeScreen.java
@@ -1,10 +1,9 @@
 package com.example.ayush.missionias;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,9 +12,7 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -45,15 +42,6 @@ public class HomeScreen extends AppCompatActivity  implements NavigationView.OnN
         setSupportActionBar(toolbar);
         names=new ArrayList<>();
         recyclerView=findViewById(R.id.recv_content);
-        //linearLayoutManager=new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -144,27 +132,7 @@ public class HomeScreen extends AppCompatActivity  implements NavigationView.OnN
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home_screen, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -181,7 +149,12 @@ public class HomeScreen extends AppCompatActivity  implements NavigationView.OnN
         } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
-
+            Intent sharingIntent=new Intent(Intent.ACTION_SEND);
+            sharingIntent.setType("text/plain");
+            String shareBody="here is share content";
+            sharingIntent.putExtra(Intent.EXTRA_SUBJECT,"Subject Here");
+            sharingIntent.putExtra(Intent.EXTRA_TEXT,shareBody);
+            startActivity(Intent.createChooser(sharingIntent,"Share via"));
         } else if (id == R.id.nav_send) {
 
         }
